3d814028242cccd699c645d4285946810fe07878
massongit <suzukimasaya428@gmail.com>
2024-02-10 16:52:24 +0900

    super-linterによる指摘事項修正

--- scripts/pr_copy_ci_sudden_death/pr_copy_ci/copy_ci.sh
+++ scripts/pr_copy_ci_sudden_death/pr_copy_ci/copy_ci.sh
@@ -2,23 +2,23 @@
 
 workflows_path=.github/workflows
 find sudden-death/${workflows_path} -type f \
-  -not -name "*sudden-death.yml" \
-  -exec rm -f {} \;
+	-not -name "*sudden-death.yml" \
+	-exec rm -f {} \;
 
 for f in $(find hato-bot/${workflows_path} -type f \
-  -not -name "*hato-bot.yml" | sed -e "s:hato-bot/${workflows_path}/::g"); do
-  yq '(.jobs.*.steps.[] | select(has("with")).with | select(has("repo-name")).repo-name) = "dev-hato/sudden-death"' "hato-bot/${workflows_path}/${f}" >"sudden-death/${workflows_path}/${f}"
+	-not -name "*hato-bot.yml" | sed -e "s:hato-bot/${workflows_path}/::g"); do
+	yq '(.jobs.*.steps.[] | select(has("with")).with | select(has("repo-name")).repo-name) = "dev-hato/sudden-death"' "hato-bot/${workflows_path}/${f}" >"sudden-death/${workflows_path}/${f}"
 done
 
 for f in $(find hato-bot/scripts -type f | grep -v hato_bot | sed -e "s:hato-bot/::g"); do
-  mkdir -p "sudden-death/$(dirname "${f}")"
-  rm -rf "sudden-death/${f}"
-  cp "hato-bot/${f}" "sudden-death/${f}"
+	mkdir -p "sudden-death/$(dirname "${f}")"
+	rm -rf "sudden-death/${f}"
+	cp "hato-bot/${f}" "sudden-death/${f}"
 done
 
 for f in .markdown-lint.yml .python-lint .textlintrc .gitleaks.toml .mypy.ini .pre-commit-config.yaml .python-version .pep8 .flake8 .python-black .isort.cfg renovate.json; do
-  rm -f "sudden-death/${f}"
-  cp hato-bot/${f} sudden-death/
+	rm -f "sudden-death/${f}"
+	cp hato-bot/${f} sudden-death/
 done
 PATTERN_BEFORE="$(grep '^click' sudden-death/Pipfile)"
 PATTERN_AFTER="$(grep '^click' hato-bot/Pipfile)"

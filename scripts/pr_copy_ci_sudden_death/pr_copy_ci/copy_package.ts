1b9297fc92728f507a7cb4431f3fd3500b49c821
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-20 13:02:55 +0900

    Add ignore comment

--- scripts/pr_copy_ci_sudden_death/pr_copy_ci/copy_package.ts
+++ scripts/pr_copy_ci_sudden_death/pr_copy_ci/copy_package.ts
@@ -1,15 +1,20 @@
 import { readFileSync, writeFileSync } from "fs";
 
 export default function script() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const hatoBotPackage: { [key: string]: any } = JSON.parse(
     readFileSync(
       `${process.env.GITHUB_WORKSPACE}/hato-bot/package.json`,
     ).toString(),
   );
+
   const suddenDeathPackagePath = `${process.env.GITHUB_WORKSPACE}/sudden-death/package.json`;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const suddenDeathPackage: { [key: string]: any } = JSON.parse(
     readFileSync(suddenDeathPackagePath).toString(),
   );
+
   delete hatoBotPackage.scripts;
 
   for (const packageKey of Object.keys(hatoBotPackage)) {
@@ -22,15 +27,21 @@ export default function script() {
     "utf8",
   );
 
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const hatoBotPackageLock: { [key: string]: any } = JSON.parse(
     readFileSync(
       `${process.env.GITHUB_WORKSPACE}/hato-bot/package-lock.json`,
     ).toString(),
   );
+
   const suddenDeathPackageLockPath = `${process.env.GITHUB_WORKSPACE}/sudden-death/package-lock.json`;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const suddenDeathPackageLock: { [key: string]: any } = JSON.parse(
     readFileSync(suddenDeathPackageLockPath).toString(),
   );
+
   delete hatoBotPackageLock.name;
 
   for (const packageLockKey of Object.keys(hatoBotPackageLock)) {

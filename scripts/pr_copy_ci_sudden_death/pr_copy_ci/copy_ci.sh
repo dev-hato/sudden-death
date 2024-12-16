3699bdd65003b1443c5f130de7de2153d2e1da44
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-12-16 20:34:01 +0900

    CI pr-copy-ci-sudden-death: scriptsコピー前に削除する

--- scripts/pr_copy_ci_sudden_death/pr_copy_ci/copy_ci.sh
+++ scripts/pr_copy_ci_sudden_death/pr_copy_ci/copy_ci.sh
@@ -9,7 +9,9 @@ for f in $(find hato-bot/${workflows_path} -type f \
 	-not -name "*hato-bot.yml" | sed -e "s:hato-bot/${workflows_path}/::g"); do
 	yq '(.jobs.*.steps.[] | select(has("with")).with | select(has("repo-name")).repo-name) = "dev-hato/sudden-death"' "hato-bot/${workflows_path}/${f}" >"sudden-death/${workflows_path}/${f}"
 done
-
+for f in $(find sudden-death/scripts -type f | grep -v sudden_death); do
+	rm -rf "$f"
+done
 for f in $(find hato-bot/scripts -type f | grep -v hato_bot | sed -e "s:hato-bot/::g"); do
 	mkdir -p "sudden-death/$(dirname "${f}")"
 	rm -rf "sudden-death/${f}"

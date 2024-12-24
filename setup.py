a00c5e2e48090ebb7f67c2d74e055991c5e67478
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-12-25 07:30:13 +0900

    setup.pyを外部パッケージに依存しない形にする

--- setup.py
+++ setup.py
@@ -2,14 +2,33 @@
 パッケージインストール用スクリプト
 """
 
-import toml
+import re
 from setuptools import setup
 
 
 def _requires_from_file(filename):
+    is_in_packages = False
+    requires = []
+
     with open(filename, encoding="UTF-8") as _f:
-        pyproject = toml.load(_f)
-        return pyproject["project"]["dependencies"]
+        for _r in _f:
+            _r = _r.strip()
+            if _r == "[project]":
+                is_in_packages = True
+            elif _r.startswith("["):
+                is_in_packages = False
+            elif is_in_packages and _r.startswith("dependencies "):
+                dep_data = _r.split("=", 1)
+
+                if len(dep_data)==0:
+                    return requires
+
+                for package in re.sub(r'[\["\]]',"", dep_data[1]).split(","):
+                    package = package.strip()
+                    if package:
+                        requires.append(package)
+
+    return requires
 
 
 setup(

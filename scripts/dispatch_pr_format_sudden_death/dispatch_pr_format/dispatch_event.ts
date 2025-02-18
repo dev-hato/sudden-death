f78e3b6c5b704595830ba99a644f351b8bd72117
Masaya Suzuki <suzukimasaya428@gmail.com>
2025-02-19 08:48:41 +0900

    masterへのpush時にhato-botのCIを呼び出すCI追加

---
+++ scripts/dispatch_pr_format_sudden_death/dispatch_pr_format/dispatch_event.ts
@@ -0,0 +1,21 @@
+import type { Context } from "@actions/github/lib/context";
+import type { GitHub } from "@actions/github/lib/utils";
+import type { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
+
+export async function script(
+  github: InstanceType<typeof GitHub>,
+  context: Context,
+) {
+  const actionsCreateWorkflowDispatchParams: RestEndpointMethodTypes["actions"]["createWorkflowDispatch"]["parameters"] =
+    {
+      owner: context.repo.owner,
+      repo: "hato-bot",
+      workflow_id: '.github/workflows/pr-format.yml',
+      ref: "master",
+    };
+  console.log("call actions.createWorkflowDispatch:");
+  console.log(actionsCreateWorkflowDispatchParams);
+  await github.rest.actions.createWorkflowDispatch(
+    actionsCreateWorkflowDispatchParams,
+  );
+}
